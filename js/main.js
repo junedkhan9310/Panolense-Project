21f351b462712756949c97a7c80acac4ae4d4de5
juned khan <junedkhanfarh@gmail.com>
2024-08-02 19:03:20 +0530

    added third image and understand 3d cordinate working

--- js/main.js
+++ js/main.js
@@ -15,10 +15,13 @@ const panorama3= new PANOLENS.ImagePanorama('images/pano2.jpg');
 let imageContainer= document.querySelector('.image-container');
 
 
-const vecotr= new THREE.Vector3(-3136.06, 296.30, -4290.14)
-const vecotr2=  new THREE.Vector3(1278.27, 732.65, 4769.19)
 
-const initialLookposition= new THREE.Vector3(1278.27, 732.65, 4769.19);
+const lookatposition =[
+  new THREE.Vector3(-3136.06, 296.30, -4290.14),
+  new THREE.Vector3(1278.27, 732.65, 4769.19)
+];
+
+const initialLookposition= new THREE.Vector3(-3278.27, 732.65, 4769.19);
 const viewer = new PANOLENS.Viewer({
   container :imageContainer,
   controlBar:false,
@@ -28,7 +31,7 @@ const viewer = new PANOLENS.Viewer({
 
 
 var infospotPositions= [
-    new THREE.Vector3(-2136.06, 16.30, 890.14),
+    new THREE.Vector3(-2136.06, 16.30, -2300.14),
     new THREE.Vector3(-3136.06, 296.30, -4290.14),
 ];
 
@@ -39,10 +42,10 @@ var infospotPositions= [
 
 infospot = new PANOLENS.Infospot( 500, PANOLENS.DataImage.Info );
 infospot.position.set( -2136.06, 16.30, 890.14);
-infospot.addHoverText( "The Story" );
+infospot.addHoverText( "Go to 2nd Image" );
 infospot.addEventListener( 'click', function(){
   viewer.setPanorama( panorama2 );
-  viewer.tweenControlCenter(vecotr2)
+  viewer.tweenControlCenter(lookatposition[0])
 
 } );
 
@@ -52,18 +55,44 @@ panorama.add( infospot );
 
 infospot2= new PANOLENS.Infospot(500,PANOLENS.DataImage.info);
 infospot2.position.set(-3136.06, 296.30, -4290.14)
-infospot2.addHoverText("This is it");
+infospot2.addHoverText("GO further");
 infospot2.addEventListener('click',function(){
+  viewer.setPanorama(panorama3);
+  viewer.tweenControlCenter(lookatposition[1])
+
+});
+
+infospot2_return= new PANOLENS.Infospot(500,PANOLENS.DataImage.info);
+infospot2_return.position.set(5000.06, 350.30, -4290.14)
+infospot2_return.addHoverText("Return to First");
+infospot2_return.addEventListener('click',function(){
   viewer.setPanorama(panorama);
-  viewer.tweenControlCenter(vecotr)
+  viewer.tweenControlCenter(lookatposition[1])
 
 })
+panorama2.add(infospot2,infospot2_return);
 
-panorama2.add(infospot2);
+
+// infospot3= new PANOLENS.Infospot(500,PANOLENS.DataImage.info);
+// infospot3.position.set(-3136.06, 296.30, -4290.14)
+// infospot3.addHoverText("3rd Image")
+// infospot3.addEventListener('click',function(){
+//   viewer.setPanorama(panorama);
+// })
+infospot3_return= new PANOLENS.Infospot(500,PANOLENS.DataImage.info);
+infospot3_return.position.set(1278.27, 732.65, 4769.19)
+infospot3_return.addHoverText("Back to 2nd image")
+infospot3_return.addEventListener('click',function(){
+  viewer.setPanorama(panorama2);
+  viewer.tweenControlCenter(lookatposition[1])
+})
+panorama3.add(infospot3_return)
+
+
 
 
 
 // panorama.link(panorama2,infospotPositions[0]);
 // panorama2.link(panorama,infospotPositions[1]);
 
-viewer.add(panorama,panorama2)
+viewer.add(panorama,panorama2,panorama3)
